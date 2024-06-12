77e1181fa7371330fa9eb0ff17ae9e66e221884a
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-06-12 17:53:12 +0600

    4.5-completed

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const maxLikes = Math.max(...blogs.map((blog) => blog.likes));
+
+  const blog = blogs.filter((blog) => blog.likes === maxLikes)[0];
+
+  return { title: blog.title, author: blog.author, likes: blog.likes };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };

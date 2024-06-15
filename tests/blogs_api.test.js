5b9797c44039f655963b3d4e6453c193586a4e53
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-06-15 14:15:53 +0600

    4.8-completed

---
+++ tests/blogs_api.test.js
@@ -0,0 +1,31 @@
+const { test, after, beforeEach } = require("node:test");
+const app = require("../app");
+const supertest = require("supertest");
+const helper = require("./test_helper");
+const assert = require("node:assert");
+const Blog = require("../models/blog");
+const mongoose = require("mongoose");
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+
+  await Promise.all(promiseArray);
+});
+
+test("returning all the blogs in json format", async () => {
+  const response = await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length);
+});
+
+after(async () => {
+  mongoose.connection.close();
+});

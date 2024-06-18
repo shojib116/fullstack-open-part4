f973a67186a77eb8e8f425a6101d9a6545856c18
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-06-18 23:22:25 +0600

    4.17-completed

--- controller/blog.js
+++ controller/blog.js
@@ -1,15 +1,30 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+  const body = request.body;
 
+  const res = await User.find({});
+  const user = res[0];
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id,
+  });
+  console.log(user);
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
   response.status(201).send(savedBlog);
 });
 
--- controller/user.js
+++ controller/user.js
@@ -3,7 +3,11 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
 userRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+  });
 
   response.json(users);
 });

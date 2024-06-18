91f21a8ff7487fc551677482b04630676b0fcd85
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-06-18 14:35:18 +0600

    4.15-completed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("express-async-errors");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controller/blog");
+const userRouter = require("./controller/user");
 const middleware = require("./utils/middleware");
 
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(middleware.modifiedMorgan);
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", userRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
---
+++ controller/user.js
@@ -0,0 +1,28 @@
+const userRouter = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+userRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+
+  response.json(users);
+});
+
+userRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
+});
+
+module.exports = userRouter;
